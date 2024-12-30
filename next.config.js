d6cbf79f815a75108d1d483bd0ff4801b451b15f
blackBl00d <121297372+blackBl00d@users.noreply.github.com>
2024-12-30 20:48:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    // This will allow Next.js to read the PORT environment variable
+    customServer: true,
+  }
 }
 
 module.exports = nextConfig
